995298088e5bc65af2e755b240701117f4c52f1d
Yesarela Ritonga <yezarela@gmail.com>
2021-05-29 13:29:59 +0700

    feat: ui improvement

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -6,6 +6,7 @@
 // images references in the manifest
 import "../../assets/icon-16.png";
 import "../../assets/icon-32.png";
+import "../../assets/icon-64.png";
 import "../../assets/icon-80.png";
 
 import * as transposer from "../transposer"
